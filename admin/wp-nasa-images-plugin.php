b44518ed5221e296f65172e9bc60431f7eed2e1b
Adilet <adiletkamchibekov@gmail.com>
2020-10-02 20:51:05 +0900

    Finished

--- admin/wp-nasa-images-plugin.php
+++ admin/wp-nasa-images-plugin.php
@@ -1,107 +1,187 @@
 <?php
 
-add_action('add_option_nasa_images_option_name', function ($option_name, $option_value) {
-    if ($option_value) {
-        $response = Requests::request('https://api.nasa.gov/planetary/apod', [
-            'api_key' => $option_value,
-            'date' => date('Y-M-D', strtotime('today'))
+add_action('admin_menu', 'nasa_images_add_plugin_page');
+
+function nasa_images_add_plugin_page()
+{
+    add_menu_page(
+        'Nasa Images', // page_title
+        'Nasa Images', // menu_title
+        'manage_options', // capability
+        'nasa-images', // menu_slug
+        'nasa_images_create_admin_page', // function
+        'dashicons-admin-generic', // icon_url
+    );
+}
+
+function nasa_images_pull_task_func()
+{
+    $api_key = get_transient('nasa_images_api');
+    if ($api_key) {
+        $image = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' =>  $api_key,
+            'date' => date('Y-m-d', strtotime('now'))
         ]);
 
-    }
-}, 10, 2);
-
-class NasaImages
-{
-    private $nasa_images_options;
-
-    public function __construct()
-    {
-        add_action('admin_menu', array($this, 'nasa_images_add_plugin_page'));
-        add_action('admin_init', array($this, 'nasa_images_page_init'));
-    }
-
-    public function nasa_images_add_plugin_page()
-    {
-        add_menu_page(
-            'Nasa Images', // page_title
-            'Nasa Images', // menu_title
-            'manage_options', // capability
-            'nasa-images', // menu_slug
-            array($this, 'nasa_images_create_admin_page'), // function
-            'dashicons-admin-generic', // icon_url
-        );
-    }
-
-    public function nasa_images_create_admin_page()
-    {
-        $this->nasa_images_options = get_option('nasa_images_option_name'); ?>
-
-        <div class="wrap">
-            <h2>Nasa Images</h2>
-            <p>Please upload your Nasa API key at first</p>
-            <?php settings_errors(); ?>
-
-            <form method="post" action="options.php">
-                <?php
-                settings_fields('nasa_images_option_group');
-                do_settings_sections('nasa-images-admin');
-                submit_button();
-                ?>
-            </form>
-        </div>
-<?php }
-
-    public function nasa_images_page_init()
-    {
-        register_setting(
-            'nasa_images_option_group', // option_group
-            'nasa_images_option_name', // option_name
-            array($this, 'nasa_images_sanitize') // sanitize_callback
+        $post = json_decode($image->body, true);
+        // Create post object
+        $my_post = array(
+            'post_title'    => wp_strip_all_tags($post['title']) . ' by ' . $post['copyright'],
+            'post_content'  => $post['explanation'],
+            'post_status'   => 'publish',
+            'post_date'     => $post['date'],
+            'post_type'     => 'post-nasa-gallery'
         );
 
-        add_settings_section(
-            'nasa_images_setting_section', // id
-            'Settings', // title
-            array($this, 'nasa_images_section_info'), // callback
-            'nasa-images-admin' // page
-        );
+        // Insert the post into the database
+        $post_id = wp_insert_post($my_post);
 
-        add_settings_field(
-            'api_key_0', // id
-            'API Key', // title
-            array($this, 'api_key_0_callback'), // callback
-            'nasa-images-admin', // page
-            'nasa_images_setting_section' // section
-        );
-    }
-
-    public function nasa_images_sanitize($input)
-    {
-        $sanitary_values = array();
-        if (isset($input['api_key_0'])) {
-            $sanitary_values['api_key_0'] = sanitize_text_field($input['api_key_0']);
-        }
-
-        return $sanitary_values;
-    }
-
-    public function nasa_images_section_info()
-    {
-    }
-
-    public function api_key_0_callback()
-    {
-        printf(
-            '<input class="regular-text" type="text" name="nasa_images_option_name[api_key_0]" id="api_key_0" value="%s">',
-            isset($this->nasa_images_options['api_key_0']) ? esc_attr($this->nasa_images_options['api_key_0']) : ''
-        );
+        // set featured image
+        setFeaturedImage($post_id, $post['url']);
+    } else {
+        wp_clear_scheduled_hook('nasa_images_pull_task');
     }
 }
 
-$nasa_images = new NasaImages();
+function startDailyCron()
+{
+    if (!wp_next_scheduled('nasa_images_pull_task')) {
+        wp_schedule_event(time(), 'daily', 'nasa_images_pull_task');
+    }
+    add_action('nasa_images_pull_task', 'nasa_images_pull_task_func');
+}
 
-/* 
- * Retrieve this value with:
- * $nasa_images_options = get_option( 'nasa_images_option_name' ); // Array of All Options
- * $api_key_0 = $nasa_images_options['api_key_0']; // API Key
- */
+function setFeaturedImage($post_id, $url)
+{
+    // Add Featured Image to Post
+    $image_name       = 'wp-nasa-image.jpg';
+    $upload_dir       = wp_upload_dir(); // Set upload folder
+    $image_data       = file_get_contents($url); // Get image data
+    $unique_file_name = wp_unique_filename($upload_dir['path'], $image_name); // Generate unique name
+    $filename         = basename($unique_file_name); // Create image file name
+
+    // Check folder permission and define file location
+    if (wp_mkdir_p($upload_dir['path'])) {
+        $file = $upload_dir['path'] . '/' . $filename;
+    } else {
+        $file = $upload_dir['basedir'] . '/' . $filename;
+    }
+
+    // Create the image  file on the server
+    file_put_contents($file, $image_data);
+
+    // Check image file type
+    $wp_filetype = wp_check_filetype($filename, null);
+
+    // Set attachment data
+    $attachment = array(
+        'post_mime_type' => $wp_filetype['type'],
+        'post_title'     => sanitize_file_name($filename),
+        'post_status'    => 'inherit'
+    );
+
+    // Create the attachment
+    $attach_id = wp_insert_attachment($attachment, $file, $post_id);
+
+    // Include image.php
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+    // Define attachment metadata
+    $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+
+    // Assign metadata to attachment
+    wp_update_attachment_metadata($attach_id, $attach_data);
+
+    // And finally assign featured image to post
+    set_post_thumbnail($post_id, $attach_id);
+}
+
+function load5images($api_key)
+{
+    if ($api_key) {
+        $images[] = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' => $api_key,
+            'date' => date('Y-m-d', strtotime('now'))
+        ]);
+        $images[] = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' => $api_key,
+            'date' => date('Y-m-d', strtotime('-1 day'))
+        ]);
+        $images[] = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' => $api_key,
+            'date' => date('Y-m-d', strtotime('-2 days'))
+        ]);
+        $images[] = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' => $api_key,
+            'date' => date('Y-m-d', strtotime('-3 days'))
+        ]);
+        $images[] = Requests::request('https://api.nasa.gov/planetary/apod', null, [
+            'api_key' => $api_key,
+            'date' => date('Y-m-d', strtotime('-4 days'))
+        ]);
+
+        foreach ($images as $image) {
+
+            $post = json_decode($image->body, true);
+            // Create post object
+            $my_post = array(
+                'post_title'    => wp_strip_all_tags($post['title']) . ' by ' . $post['copyright'],
+                'post_content'  => $post['explanation'],
+                'post_status'   => 'publish',
+                'post_date'     => $post['date'],
+                'post_type'     => 'post-nasa-gallery'
+            );
+
+            // Insert the post into the database
+            $post_id = wp_insert_post($my_post);
+
+            // set featured image
+            setFeaturedImage($post_id, $post['url']);
+        }
+    }
+}
+
+
+function nasa_images_create_admin_page()
+{
+    $nasa_images_api = get_transient('nasa_images_api'); ?>
+
+    <div class="wrap">
+        <h2>Nasa Images</h2>
+        <p>Please upload your Nasa Images API key at first</p>
+
+        <?php if (isset($_POST['nasa_images_api'])) :
+
+            if (!get_option('nasa-images-5loaded')) {
+                load5images($_POST['nasa_images_api']);
+                add_option('nasa-images-5loaded', true);
+            }
+            if (!wp_next_scheduled('nasa_images_pull_task')) {
+                startDailyCron();
+            }
+
+            $nasa_images_api = esc_sql($_POST['nasa_images_api']);
+            set_transient('nasa_images_api', $nasa_images_api, 60 * 60 * 24 * 90)
+        ?>
+            <div class="notice notice-success">
+                <p> Api Key successfully saved </p>
+            </div>
+        <?php endif ?>
+
+        <form method="post">
+            <table class="form-table">
+                <tbody>
+                    <th scope="row">
+                        <label for="nasa-images-api">API Key: </label>
+                    </th>
+                    <td>
+                        <input type="text" value="<?php echo $nasa_images_api; ?>" id="nasa-images-api" name="nasa_images_api">
+                    </td>
+                </tbody>
+            </table>
+            <?php
+            submit_button();
+            ?>
+        </form>
+    </div>
+<?php }
--- wp-nasa-images-plugin.php
+++ wp-nasa-images-plugin.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
-* Plugin Name: Nasa images
-* Description: Shows images from Nasa imagery
-* Version: 1.0
-* Author: Adilet Kamchibekov
-* Author URI: https://github.com/kamchibekov
-* License: GPL v2 or later
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Text Domain: wp-nasa-images-plugin
-*/
+ * Plugin Name: Nasa images
+ * Description: Shows images from Nasa imagery
+ * Version: 1.0
+ * Author: Adilet Kamchibekov
+ * Author URI: https://github.com/kamchibekov
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-nasa-images-plugin
+ */
 
 
 // Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
-	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-	exit;
+if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
 }
 
 /**
@@ -23,7 +23,10 @@ if ( !function_exists( 'add_action' ) ) {
  */
 function pluginprefix_setup_post_type()
 {
-    register_post_type('post-nasa-gallery', ['public' => true]);
+    register_post_type('post-nasa-gallery', [
+        'label' => 'Nasa Images Posts',
+        'public' => true
+    ]);
 }
 add_action('init', 'pluginprefix_setup_post_type');
 
@@ -49,6 +52,18 @@ function pluginprefix_deactivate()
     unregister_post_type('post-nasa-gallery');
     // Clear the permalinks to remove our post type's rules from the database.
     flush_rewrite_rules();
+
+    // remove cron task
+    wp_clear_scheduled_hook('nasa_images_pull_task');
+
+    // Don't remove options and transients
+    //delete_option('nasa-images-5loaded');
+    //delete_transient('nasa_images_api');
+
+    // hide the sortcode incase if not deleted
+    add_shortcode('nasa-images-gallery', function () {
+        return null;
+    });
 }
 register_deactivation_hook(__FILE__, 'pluginprefix_deactivate');
 
@@ -56,4 +71,53 @@ register_deactivation_hook(__FILE__, 'pluginprefix_deactivate');
 if (is_admin()) {
     // we are in admin mode
     require_once __DIR__ . '/admin/wp-nasa-images-plugin.php';
+} else {
+    add_action("wp_enqueue_scripts", "myscripts");
+
+    function myscripts()
+    {
+        wp_enqueue_script('jquery');
+        wp_enqueue_style('slick-theme', plugins_url('/public/css/slick-theme.css', __FILE__));
+        wp_enqueue_style('slick-css', plugins_url('/public/css/slick.css', __FILE__));
+        wp_enqueue_script('slick-js',  plugins_url('/public/js/slick.min.js', __FILE__));
+    }
 }
+
+
+//[nasa-images-gallery]
+function get_gallery()
+{
+    ob_start();
+    $args = array(
+        'post_type' => 'post-nasa-gallery',
+    );
+
+    $the_query = new WP_Query($args);
+    if ($the_query->have_posts()) : ?>
+        <section class="nasa-images-gallery slider">
+            <?php
+            while ($the_query->have_posts()) :
+                $the_query->the_post(); ?>
+
+                <div>
+                    <img src="<?php echo get_the_post_thumbnail_url(null, [500, 500]) ?>">
+                </div>
+
+
+            <?php endwhile; ?>
+        </section>
+        <script type="text/javascript">
+            jQuery(".nasa-images-gallery").slick({
+                dots: true,
+                infinite: true,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear'
+            });
+        </script>
+<?php wp_reset_postdata();
+    else :
+    endif;
+    return ob_get_clean();
+}
+add_shortcode('nasa-images-gallery', 'get_gallery');
